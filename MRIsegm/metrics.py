00c309642fa9b9edb995636ef3721850dd755019
Giuseppe Filitto <giuseppefilitto@gmail.com>
2021-04-13 16:26:37 +0200

    changed metrics implementation

--- MRIsegm/metrics.py
+++ MRIsegm/metrics.py
@@ -1,104 +1,182 @@
 import tensorflow as tf
+import tensorflow.keras.backend as K
 
 
 __author__ = ['Giuseppe Filitto']
 __email__ = ['giuseppe.filitto@studio.unibo.it']
 
 
-def dice_coe(output, target, axis=(1, 2, 3), smooth=1e-5):
-    """
+def dice_coef(y_true, y_pred, smooth=1):
+    '''
 
-    Sørensen coefficient for comparing the similarity of two batch of data,
-    usually be used for binary image segmentation
-    i.e. labels are binary.
-    The coefficient between 0 to 1, 1 means totally match.
+    Dice coefficient, also know as Sørensen-Dice index, is used to gauge the similarity of two samples. Given 2 sets it is defined as  twice the number of elements common to both sets divided by the sum of the number of elements in each set.
 
     Parameters
-    -----------
-    output : Tensor
-        A distribution with shape: [batch_size, ....], (any dimensions).
-    target : Tensor
-        The target distribution, format the same with `output`.
-    axis : tuple of int
-         default (1,2,3).
-    smooth : float
-        This small value will be added to the numerator and denominator.
-            - If both output and target are empty, it makes sure dice is 1.
-            - If either output or target are empty (all pixels are background), dice = ```smooth/(small_value + smooth)``, then if smooth is very small, dice close to 0 (even the image values lower than the threshold), so in this case, higher smooth can have a higher dice.
+    ----------
+    y_true : Tensor
+        input tensor with shape: [batch_size, height, width, channels].
+    y_pred : Tensor
+        predicted tensor with shape: [batch_size, height, width, channels].
+    smooth : int, float, optional
+        value that will be added to the numerator and denominator to avoid 0 division, by default 1.
+
+    Returns
+    -------
+    dice: float
+        dice coefficient. The index is a number between 0 and 1 , if 1 sets totally match.
 
     References
     -----------
-    Wiki <https://en.wikipedia.org/wiki/Sørensen–Dice_coefficient>
+    - Wiki https://en.wikipedia.org/wiki/Sørensen–Dice_coefficient
 
-    """
-    inse = tf.reduce_sum(output * target, axis=axis)
 
-    l = tf.reduce_sum(output * output, axis=axis)
-    r = tf.reduce_sum(target * target, axis=axis)
+    '''
 
-    dice = (2. * inse + smooth) / (l + r + smooth)
-    dice = tf.reduce_mean(dice)
+    intersection = K.sum(y_true * y_pred, axis=[1, 2, 3])
+    total = K.sum(y_true, axis=[1, 2, 3]) + K.sum(y_pred, axis=[1, 2, 3])
+    dice = K.mean((2. * intersection + smooth) / (total + smooth), axis=0)
     return dice
 
 
-def jacc_index(output, target, axis=(1, 2, 3), smooth=1e-5):
-    """
+def iou(y_true, y_pred, smooth=1):
+    '''
 
-    jaccard index for comparing the similarity of two batch of data,
-    usually be used for binary image segmentation
-    i.e. labels are binary.
-    The coefficient between 0 to 1, 1 means totally match.
-
-    jaccard index 'J' also know as 'intersection over union' is obtained from dice coefficeint 'D' by the following formula:
-
-                            J = D / (2 - D)
-
+    Intersection over union (IoU) also know as Jaccard coefficient, measures similarity between finite sample sets. Defined as the size of the intersection divided by the size of the union of the sample sets.
 
     Parameters
-    -----------
-    output : Tensor
-        A distribution with shape: [batch_size, ....], (any dimensions).
-    target : Tensor
-        The target distribution, format the same with `output`.
-    axis : tuple of int
-         default (1,2,3).
-    smooth : float
-        This small value will be added to the numerator and denominator of the dice_coe function which the jaccard index is calculated from..
+    ----------
+    y_true : Tensor
+        input tensor with shape: [batch_size, height, width, channels].
+    y_pred : Tensor
+        predicted tensor with shape: [batch_size, height, width, channels].
+    smooth : int, float, optional
+        value that will be added to the numerator and denominator to avoid 0 division, by default 1.
+
+    Returns
+    -------
+    iou: float
+        Intersection over union. The index is a number between 0 and 1 , if 1 sets totally match.
 
     References
     -----------
-    Wiki <https://en.wikipedia.org/wiki/Jaccard_index>
-
-    """
-
-    dice = dice_coe(output, target, axis=(1, 2, 3), smooth=1e-5)
-    jacc = dice / (2 - dice)
-    return jacc
+    - Wiki https://en.wikipedia.org/wiki/Jaccard_index
 
 
-def jacc_loss(output, target, axis=(1, 2, 3), smooth=1e-5):
-    """
+    '''
 
-    The Jaccard distance, which measures dissimilarity between sample sets, is complementary to the Jaccard coefficient and is obtained by subtracting the Jaccard coefficient from 1
+    intersection = K.sum(y_true * y_pred, axis=[1, 2, 3])
+    union = K.sum(y_true, axis=[1, 2, 3]) + \
+        K.sum(y_pred, axis=[1, 2, 3]) - intersection
+    iou = K.mean((intersection + smooth) / (union + smooth), axis=0)
+    return iou
 
+
+def iou_loss(y_true, y_pred):
+    '''
+
+    Also know as Jaccard distance, measures dissimilarity between sample sets, is complementary to the Jaccard coefficient and is obtained by subtracting the Jaccard coefficient from 1, or, equivalently, by dividing the difference of the sizes of the union and the intersection of two sets by the size of the union.
 
     Parameters
-    -----------
-    output : Tensor
-        A distribution with shape: [batch_size, ....], (any dimensions).
-    target : Tensor
-        The target distribution, format the same with `output`.
-    axis : tuple of int
-         default (1,2,3).
-    smooth : float
-        This small value will be added to the numerator and denominator of the dice_coe function which the jaccard index is calculated from.
+    ----------
+    y_true : Tensor
+        input tensor with shape: [batch_size, height, width, channels].
+    y_pred : Tensor
+        predicted tensor with shape: [batch_size, height, width, channels].
 
     References
     -----------
-    Wiki <https://en.wikipedia.org/wiki/Jaccard_index>
+    - Wiki https://en.wikipedia.org/wiki/Jaccard_index
 
-    """
 
-    jacc = jacc_index(output, target, axis=(1, 2, 3), smooth=1e-5)
+    '''
 
-    return 1 - jacc
+    iou = iou(y_true, y_pred)
+    return 1 - iou
+
+
+def tversky_index(y_true, y_pred, smooth=1, alpha=0.7):
+    '''
+
+     The Tversky index is an asymmetric similarity measure on sets that compares a variant to a prototype. The Tversky index can be seen as a generalization of the Sørensen–Dice coefficient and the Jaccard index. Setting alpha = beta = 1 produces the jaccard coefficient, setting alpha = beta = 0.5 produces the Sørensen–Dice coefficient
+
+
+    Parameters
+    ----------
+    y_true : Tensor
+        input tensor with shape: [batch_size, height, width, channels].
+    y_pred : Tensor
+        predicted tensor with shape: [batch_size, height, width, channels].
+    smooth : int, float, optional
+        value that will be added to the numerator and denominator to avoid 0 division, by default 1.
+    alpha: float, optional
+        parameter  of the Tversky index, by default 0.7.
+
+    Returns
+    -------
+    tversky: float
+        Tversky index. The index is a number between 0 and 1 , if 1 sets totally match.
+
+    References
+    -----------
+    - Wiki https://en.wikipedia.org/wiki/Tversky_index
+
+
+    '''
+
+    beta = 1 - alpha  # since alpha + beta = 1 cases are of more interest
+
+    true_pos = K.sum(y_true * y_pred, axis=[1, 2, 3])
+    false_neg = K.sum(y_true * (1 - y_pred), axis=[1, 2, 3])
+    false_pos = K.sum((1 - y_true) * y_pred, axis=[1, 2, 3])
+    tversky = K.mean((true_pos + smooth)/(true_pos + alpha *
+                                          false_neg + beta * false_pos + smooth), axis=0)
+
+    return tversky
+
+
+def tversky_loss(y_true, y_pred):
+    '''
+
+     Measure of dissimilarity between sample sets, is complementary to the Tversky index and is obtained by subtracting the Tversky index from 1.
+
+    Parameters
+    ----------
+    y_true : Tensor
+        input tensor with shape: [batch_size, height, width, channels].
+    y_pred : Tensor
+        predicted tensor with shape: [batch_size, height, width, channels].
+
+    References
+    -----------
+    - https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=8759329
+
+    '''
+
+    T = tversky_index(y_true, y_pred)
+
+    return 1 - T
+
+
+def focal_tversky_loss(y_true, y_pred, gamma=1.33):
+    '''
+
+   Generalized focal loss function based on the Tversky index to address the issue of data imbalance in medical image segmentation.
+
+    Parameters
+    ----------
+    y_true : Tensor
+        input tensor with shape: [batch_size, height, width, channels].
+    y_pred : Tensor
+        predicted tensor with shape: [batch_size, height, width, channels].
+    gamma: float
+        control parameter, by default 1.33. Proposed in range [1,3].
+
+    References
+    -----------
+    - https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=8759329
+
+    '''
+
+    T = tversky(y_true, y_pred)
+
+    return K.pow((1-T), 1/gamma)

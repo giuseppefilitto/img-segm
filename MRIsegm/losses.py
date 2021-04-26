7898bb3a3501bb96eaf47b6d88f7967222414410
Giuseppe Filitto <giuseppefilitto@gmail.com>
2021-04-26 18:22:39 +0200

    added soft dice loss

--- MRIsegm/losses.py
+++ MRIsegm/losses.py
@@ -65,3 +65,35 @@ def DiceBCEloss(y_true, y_pred):
     dice_bce = bce(y_true, y_pred) + dice_l
 
     return dice_bce
+
+
+def soft_dice_loss(y_true, y_pred):
+    '''
+
+    Soft dice distance, measures dissimilarity between sample sets, it is complementary to the dice coefficient and it is obtained by subtracting the dice coefficient from 1. It is similar to dice loss but |X| + |Y| is obtained as y_true**2 + y_pred**2
+
+    Parameters
+    ----------
+    y_true : Tensor
+        input tensor with shape [batch_size, d0, .. dN].
+    y_pred : Tensor
+        predicted tensor with same shape of y_true.
+
+    Returns
+    -------
+    Weighted loss float Tensor
+       scalar dice loss tensor with shape=().
+
+    References
+    -----------
+    - Wiki https://en.wikipedia.org/wiki/Sørensen–Dice_coefficient
+
+    '''
+
+    smooth = 1e-5
+    intersection = tf.reduce_sum(y_true * y_pred, axis=[1, 2, 3])
+    total = tf.reduce_sum(
+        y_true**2, axis=[1, 2, 3]) + tf.reduce_sum(y_pred**2, axis=[1, 2, 3])
+    dice = tf.reduce_mean((2. * intersection + smooth) / (total + smooth))
+
+    return 1 - dice

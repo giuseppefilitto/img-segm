88c7ed0c80e6cba542b2a863f1f565d3c55c8873
Giuseppe Filitto <giuseppefilitto@gmail.com>
2021-05-04 15:13:37 +0200

    re-arranged module

--- MRIsegm/graphics.py
+++ MRIsegm/graphics.py
@@ -1,49 +1,9 @@
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 __author__ = ['Giuseppe Filitto']
 __email__ = ['giuseppe.filitto@studio.unibo.it']
-
-
-def create_segmentation_generator(img_path, mask_path, BATCH_SIZE, IMG_SIZE, SEED, data_gen_args_img, data_gen_args_mask):
-    '''
-
-    Create DataGenerator yielding tuples of (x, y) with shape (batch_size, height, width, channels) where x is the input image and y is the true mask. The data generation is performed using data_gen_args_img and data_gen_args_mask.
-
-    Parameters
-    ----------
-    img_path : str
-        path for the images directory.
-    mask_path : str
-        path for the masks directory.
-    BATCH_SIZE : int
-        size of the batches of data.
-    IMG_SIZE : tuple
-        (image height, image width).
-    SEED : int
-        seed for randomness control.
-    data_gen_args_img: dict
-        dict of keras ImageDataGenerator args for the generation of custom images
-    data_gen_args_mask: dict
-        dict of keras ImageDataGenerator args for the generation of custom masks
-    Returns
-    -------
-    tuples
-        tuples of (x, y) with shape (batch_size, height, width, channels) where x is the input image and y is the true mask.
-
-    '''
-
-    img_data_gen = ImageDataGenerator(**data_gen_args_img)
-    mask_data_gen = ImageDataGenerator(**data_gen_args_mask)
-
-    img_generator = img_data_gen.flow_from_directory(
-        img_path, target_size=IMG_SIZE, class_mode=None, color_mode='grayscale', batch_size=BATCH_SIZE, seed=SEED)
-    mask_generator = mask_data_gen.flow_from_directory(
-        mask_path, target_size=IMG_SIZE, class_mode=None, color_mode='grayscale', batch_size=BATCH_SIZE, seed=SEED)
-
-    return zip(img_generator, mask_generator)
 
 
 def display(display_list, colormap=False, cmap='gist_heat', norm=True):

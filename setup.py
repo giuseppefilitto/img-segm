1445ee1cbcb5843c8d245a9c94c3b7c3de16d050
Giuseppe Filitto <giuseppefilitto@gmail.com>
2021-07-31 19:30:04 +0200

    created files for travis CI

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name="img-segm",
+    version="0.0.1",
+
+    author="giuseppefilitto",
+    author_email="giuseppefilitto@gmail.com",
+
+    url="https://github.com/giuseppefilitto/img-segm.git",
+
+    packages=find_packages(),
+
+    python_requires=">=3.9"
+
+)

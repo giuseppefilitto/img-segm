49f13255d2a7268104a8cd0c9fc53741b49d6938
Giuseppe Filitto <giuseppefilitto@gmail.com>
2021-08-14 12:34:07 +0200

    added new docstrings

--- MRIsegm/graphics.py
+++ MRIsegm/graphics.py
@@ -10,23 +10,23 @@ __email__ = ['giuseppe.filitto@studio.unibo.it']
 def display(display_list, colormap=False, cmap='gist_heat', norm=True):
     '''
 
-    Display a list of images: 'Input image', 'True Mask', 'Predicted mask'. It is possibile to show the predicted mask using matplotlib cmap setting colormap as True and if norm is set as True the image is normalized from 0. to 1.
+    Display a list of images: 'Input image', 'Ground-truth', 'Predicted'. It is possible to show the predicted image using a matplotlib cmap setting colormap as True.
 
     Parameters
     ----------
     display_list : list
-        list of input image, true mask and predicted mask.
+        list of input images: ground-truth and predicted image.
     colormap : bool, optional
-        if True show the predicted mask using a seqeuntial colormap (i.e. matplotlib 'gist_heat'), by default False.
+        if True show the predicted image using a sequential colormap (i.e. matplotlib 'gist_heat'), by default False.
     cmap: str
         matplotlib cmap, by default 'gist_heat'.
     norm: bool, optional
-        if normalized the predicted mask from 0. to 1., by deafult True.
+        if normalized the predicted image values are in the range [0.-1.], by deafult True.
 
     '''
     plt.style.use('default')
     plt.figure(figsize=(12, 8))
-    title = ['Input image', 'True Mask', 'Predicted mask']
+    title = ['Input image', 'Ground-truth', 'Predicted']
     for i in range(len(display_list)):
         plt.subplot(1, len(display_list), i + 1)
         plt.title(title[i])
@@ -56,12 +56,13 @@ def display(display_list, colormap=False, cmap='gist_heat', norm=True):
 def show_dataset(datagen, num=1):
     '''
 
-    Show a given number of the first of a batch of data created using datagenerator (i.e. create_segmentation_generator_*).
+    Show a given number 'num' of tuples of images created using datagenerator (i.e. create_segmentation_generator).
+    The first one is the input image, the second ine is the relative ground-truth.
 
     Parameters
     ----------
     datagen : iterator
-         DirectoryIterator yielding tuples of (x, y) with shape (batch_size, height, width, channels) where x is the input image and y is the true mask.
+         DirectoryIterator yielding tuples of (x, y) images with shape (batch_size, height, width, channels) where x is the input image and y is the relative ground-truth image.
     num : int, optional
         number of (x, y) tuples to be shown, by default 1.
 
@@ -75,24 +76,24 @@ def show_dataset(datagen, num=1):
 def plot_history(model_name, history, metrics, loss, save=True, custom_metrics=True, custom_loss=False, **kwargs):
     '''
 
-    Show the model history such as metrics and loss for both training and validation using 'seaborn' style from matplotlib.
+    Show the model history such as metrics and loss for both training and validation using 'seaborn' style.
 
     Parameters
     ----------
     model_name : str
         name of the model. It is the suptitle of the plot.
-    history : tf.keras History object
-        result of model.fit(). it is a record of training loss values and metrics values at successive epochs, as well as validation loss values and validation metrics values (if applicable).
+    history : Keras History object
+        result of model.fit(). It is a record of training loss values and metrics values at successive epochs, as well as validation loss values and validation metrics values.
     metrics : list
-        List of metrics to be evaluated by the model during training and testing. Each of this must be a string (name of a built-in function) or custom function (see MRIsegm.metrics).
+        List of metrics to be evaluated by the model during training and testing. Each of this must be a string (name of a built-in function) or custom function (see 'MRIsegm.metrics').
     loss : string or a function
-        string of name of a built-in function (i.e. 'binary_crossentropy' from tf.keras.metrics) or a custom function (see MRIsegm.losses).
+        string of name of a built-in function (i.e. 'binary_crossentropy' from tf.keras.metrics) or a custom function (see 'MRIsegm.losses').
     save : bool, optional
         if saving the plot to the given path which must be given as path='path/to/plot', by default True.
     custom_metrics : bool, optional
-        if metrics are all custom function (see MRIsegm.metrics) or not (i.e. 'accuracy' from tf.keras.metrics), by default True.
+        if 'True' consider metrics as a custom function (see 'MRIsegm.metrics'), by default True.
     custom_loss : bool, optional
-        if loss is a custom function (see MRIsegm.losses) or not (i.e. 'binary_crossentropy' from tf.keras.metrics), by default False.
+        if 'True' consider loss as a custom function (see 'MRIsegm.losses'), by default False.
 
     '''
 
@@ -156,22 +157,22 @@ def plot_history(model_name, history, metrics, loss, save=True, custom_metrics=T
 def show_prediction(datagen, model, num=1, colormap=True, cmap='gist_heat', norm=True):
     '''
 
-    Show a given number of [x, y, z] where x is the first input image, y the first true mask created using datagenerator (i.e create_segmentation_generator) and z is the predicted mask from x by the given model. The predicted mask is shown using a sequential colormap.
+    Show a given number 'num' of a series of image from datagenerator. The first of the series is the input image, the second one is ground-truth and the last one is the predicted image by the given model. The predicted image is shown using a sequential colormap.
 
     Parameters
     ----------
     datagen : iterator
-         DirectoryIterator yielding tuples of (x, y) with shape (batch_size, height, width, channels) where x is the input image and y is the true mask.
+         DirectoryIterator yielding tuples of (x, y) with shape (batch_size, height, width, channels) where x is the input image and y is the ground-truth.
     model : Keras Model class
-        model used to predict the mask.
+        model used to make the prediction.
     num : int, optional
-        number of rows of images to be shown, by default 1.
+        number of series to be shown, by default 1.
     colormap : bool, optional
-        if True the predicted mask is shown using a sequential colormap (i.e. matplotlib 'gist_heat'), by default True.
+        if True the prediction is shown using a sequential colormap (i.e. matplotlib 'gist_heat'), by default True.
     cmap: str
         matplotlib cmap, by default 'gist_heat'.
     norm: bool, optional
-        if normalized the predicted mask from 0. to 1., by deafult True.
+        if normalized the predicted values are in the range [0.-1], by deafult True.
 
     '''
     plt.style.use('default')
@@ -182,8 +183,25 @@ def show_prediction(datagen, model, num=1, colormap=True, cmap='gist_heat', norm
 
 
 def display_predictions(display_list, keys, colormap=True, cmap='gist_heat', figsize=(25, 15)):
+    '''
+
+    Display the ground-truth image and the predicted images from a list using a matplotlib colormap (setting colormap as True).
+
+    Parameters
+    ----------
+    display_list : list
+        list of input images: ground-truth and predicted mask.
+    keys : list
+        list of models' name used for generating the prediction.
+    colormap : bool, optional
+        if True the prediction is shown using a sequential colormap (i.e. matplotlib 'gist_heat'), by default True.
+    cmap : str, optional
+        matplotlib cmap, by default 'gist_heat'.
+    figsize : tuple, optional
+        figure size, by default (25, 15).
+    '''
     plt.figure(figsize=figsize, constrained_layout=True)
-    title = ['True Mask'] + keys
+    title = ['ground-truth'] + keys
     for i in range(len(display_list)):
         plt.subplot(1, len(display_list), i + 1)
         plt.title(title[i])
@@ -201,6 +219,27 @@ def display_predictions(display_list, keys, colormap=True, cmap='gist_heat', fig
 
 
 def show_multiple_predictions(datagen, keys, values, num=1, colormap=True, cmap='gist_heat', figsize=(25, 15)):
+    '''
+
+    Show a given number 'num' of predicted images by the given models from datagenerator. The first one is the ground-truth.Each predicted image is shown using a matplotlib sequential colormap.
+
+    Parameters
+    ----------
+    datagen : iterator
+        DirectoryIterator yielding tuples of (x, y) with shape (batch_size, height, width, channels) where x is the input image and y is the ground-truth.
+    keys : list
+        list of models' name used for generating the predictions.
+    values : list
+        list of loaded models used for generating the predictions.
+    num : int, optional
+        number of series to be shown, by default 1.
+    colormap : bool, optional
+        if True the prediction is shown using a sequential colormap (i.e. matplotlib 'gist_heat'), by default True.
+    cmap : str, optional
+        matplotlib cmap, by default 'gist_heat'.
+    figsize : tuple, optional
+        figure size, by default (25, 15).
+    '''
     for i in range(0, num):
         image, mask = next(datagen)
         pred_masks = [model.predict(image) for model in values]
@@ -210,8 +249,24 @@ def show_multiple_predictions(datagen, keys, values, num=1, colormap=True, cmap=
 
 
 def display_overlap(display_list, keys, colormap=True, cmap='gist_heat', figsize=(25, 15)):
+    '''
+    Display the ground-truth image and the predicted images from a list using a matplotlib colormap (setting colormap as True), over the original images.
+
+    Parameters
+    ----------
+    display_list : list
+        list of input images: ground-truth and predicted mask.
+    keys : list
+        list of models' name used for generating the prediction.
+    colormap : bool, optional
+        if True the predicted mask is shown using a sequential colormap (i.e. matplotlib 'gist_heat'), by default True.
+    cmap : str, optional
+        matplotlib cmap, by default 'gist_heat'.
+    figsize : tuple, optional
+        figure size, by default (25, 15).
+    '''
     plt.figure(figsize=figsize, constrained_layout=True)
-    title = ['True Mask'] + keys
+    title = ['ground-truth'] + keys
     for i in range(1, len(display_list)):
         plt.subplot(1, len(display_list), i + 1)
         plt.title(title[i - 1])
@@ -231,6 +286,27 @@ def display_overlap(display_list, keys, colormap=True, cmap='gist_heat', figsize
 
 
 def show_multiple_overlap(datagen, keys, values, num=1, colormap=True, cmap='gray', figsize=(25, 15)):
+    '''
+
+    Show a given number 'num' of predicted images by the given models from datagenerator, over the original images. The first one is the ground-truth. Each predicted image is shown using a matplotlib sequential colormap(setting colormap as 'True').
+
+    Parameters
+    ----------
+    datagen : iterator
+        DirectoryIterator yielding tuples of (x, y) with shape (batch_size, height, width, channels) where x is the input image and y is the ground-truth.
+    keys : list
+        list of models' name used for generating the prediction.
+    values : list
+        list of loaded models used for generating the predictions.
+    num : int, optional
+        number of series to be shown, by default 1.
+    colormap : bool, optional
+        if True the prediction is shown using a sequential colormap, by default True.
+    cmap : str, optional
+        matplotlib cmap, by default 'gray'.
+    figsize : tuple, optional
+        figure size, by default (25, 15).
+    '''
     for i in range(0, num):
         image, mask = next(datagen)
         pred_masks = [model.predict(image) for model in values]
